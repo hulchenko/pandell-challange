b7db99fcf9f914d1c0510d6b432e02c49d042558
Vadym Hulchenko <vadymhulchenko@gmail.com>
2022-02-19 04:15:08 -0700

    update returned values

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ function App() {
   const [array, setArray] = useState([]);
   const min = 1;
   const max = 10000;
-  const initArray = [];
-  const randomAmount = () => {
-    for (let i = min; i < max; i++) {
-      // console.log(i);
+  let initArray = [];
+
+  const randomNumbers = (initArray) => {
+    // create new array 1-10,000
+    for (let i = min; i <= max; i++) {
       initArray.push(i);
-      setArray(initArray);
     }
     // console.log(initArray);
+    let randomArray = [];
+    let randomNumber;
+    let initArrayLength = initArray.length;
+
+    while (initArrayLength) {
+      //decrease init array length with each iteration
+      randomNumber = Math.round(Math.random() * initArrayLength--);
+
+      //select random index values in init array and append to the new one
+      // console.log(`HERE: `, initArray.splice(randomNumber, 1));
+      randomArray.push(initArray.splice(randomNumber, 1)[0]);
+    }
+    setArray(randomArray);
   };
 
   return (
     <div className="App">
       <h1 style={{ textAlign: 'center' }}>1 - 10,000</h1>
       <div style={{ width: '100%', textAlign: 'center' }}>
-        <button onClick={() => randomAmount()}>GENERATE</button>
+        <button onClick={() => randomNumbers(initArray)}>GENERATE</button>
       </div>
       <div
         style={{
@@ -31,7 +44,7 @@ function App() {
       >
         {array.map((item) => {
           return (
-            <li key={item} style={{ listStyle: 'none' }}>
+            <li key={item + item} style={{ listStyle: 'none' }}>
               {item}
             </li>
           );
